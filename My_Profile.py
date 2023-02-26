f7031fcfddbc3cd80dccecf8b985c0bccf6f6628
Akshay <126151845+Akshay2515@users.noreply.github.com>
2023-02-26 23:50:04 +0530

    Add files via upload

---
+++ My_Profile.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("PROJECT_1")
+st.header(":blue[STUDENT DATA] APP")
+st.subheader(":red[Innomatics Research] labs")
+
+st.caption("Hey guys, :red[I'm Akshay]. This is my first project on streamlit. ")
+
+btn_click = st.button("click")
+
+if btn_click == True:
+    st.subheader(":red[FUTURE DATA SCIENTIST] :sunglasses:")
+    st.balloons()
+
+    
+
+    
